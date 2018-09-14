27d5c777dc7c3b835ed321dbe19b4647cb4538a7
Veronica Bustabad <vbustabad@gmail.com>
2018-09-14 02:39:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// function produceDrivingRange() {
-//   return function(blockRange) {
-//     return marketMultiplier * manufacturePrice;
-//   };
-// }
+function produceDrivingRange() {
+
+  return function(blockRange) {
+    return (blockRange[1] - blockRange[0]);
+  };
+}
 
 function produceTipCalculator() {
   let tenPercentTip = produceTipCalculator(.10)
-  
+
   return function(percentage) {
     return tenPercentTip(bill_amount) * percentage;
   };
