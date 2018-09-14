0af6eadf51c2f644923d95a44a0a634ecaf58128
Veronica Bustabad <vbustabad@gmail.com>
2018-09-14 03:06:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function produceDrivingRange(blockRange) {
-  let beginning_point = parseInt(beginning_point).slice(0, -2);
-  let end_point = parseInt(end_point).slice(0, -2);
-  
   return function(beginning_point, end_point) {
+    let beginning_point = parseInt(beginning_point).slice(0, -2);
+    let end_point = parseInt(end_point).slice(0, -2);
+    
     if ((beginning_point - end_point) > blockRange) {
       return false;
     }
