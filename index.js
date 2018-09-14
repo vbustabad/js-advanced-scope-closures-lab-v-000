5183a576972d22525d3b819dec3356d9bd0bf291
Veronica Bustabad <vbustabad@gmail.com>
2018-09-14 03:11:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(blockRange) {
   let beginning_point = parseInt(beginning_point).slice(0, -2);
   let end_point = parseInt(end_point).slice(0, -2);
-  
+
   return function(beginning_point, end_point) {
     if ((beginning_point - end_point) > blockRange) {
       return false;
@@ -18,4 +18,11 @@ function produceTipCalculator(percentage) {
   };
 }
 
-// createDriver
+function createDriver() {
+  return Driver {
+      constructor(name) {
+        this.name = name;
+        this.id = ++driverId;
+      }
+  };
+}
